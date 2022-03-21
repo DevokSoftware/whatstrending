db33362f36e3f6948cfd1b11dd3aacbfb8c05740
DevokSoftware <devok.software@gmail.com>
2022-03-21 07:56:24 +0000

    - made design responsive - changed language of tweet's date - fixed images/videos heights - images/videos slide only if it exists

--- frontend/src/app/shared/custom.date.ts
+++ frontend/src/app/shared/custom.date.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {format, formatDistance } from 'date-fns';
-import pt from "date-fns/locale/pt"
+import { format, formatDistance } from 'date-fns';
+import en from "date-fns/locale/en-US"
 
 @Pipe({
   name: 'dateFormat'
 })
 export class FormatDatePipe implements PipeTransform {
   transform(value: any): string {
-    return formatDistance(new Date(), new Date(value), {locale:pt});
+    return formatDistance(new Date(), new Date(value), { locale: en });
   }
 }
